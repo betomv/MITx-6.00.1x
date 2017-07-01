1a5e69919dbab1ee9cdba305ad587086fb552335
Rajat <rajatbapuri@gmail.com>
2017-07-01 16:00:01 +0530

    Create Problem1.py

---
+++ Week 1- Python Basics/Problem Set 1/Problem1.py
@@ -0,0 +1,7 @@
+# To count number of vowels in a string and print the same
+
+count = 0
+for letter in s:
+    if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+        count += 1
+print('Number of vowels:' + str(count))
